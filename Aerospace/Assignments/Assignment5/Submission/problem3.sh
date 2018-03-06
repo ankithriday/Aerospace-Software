814cf6c7ee17d608a956bc051dc41c12eba28a3c
ankithriday <anhr8184@colorado.edu>
2018-03-06 12:20:26 -0700

    updated problem3

--- Aerospace/Assignments/Assignment5/Submission/problem3.sh
+++ Aerospace/Assignments/Assignment5/Submission/problem3.sh
@@ -6,10 +6,10 @@ echo "Problem-3 Assignment-5"
 echo "Inputs is $1 and $2"
 
 Base=$1
-Arg=$2
+Ext=$2
 i=1
-for file in $Base/*.jpg;
+for file in $PWD/*.$2;
 do file "$file";
-mv $file $Arg$i.jpg
+mv $file $Base$i.$2
 i=$[ i + 1 ]
 done
